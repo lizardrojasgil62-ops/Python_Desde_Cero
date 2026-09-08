686641609865f9ae9c5dfc31d6d9e06f2b6c7e86
Pablo Rojas <lizardrojasgil6.2@gmail.com>
2026-09-08 02:44:00 +0200

    Add basic arithmetic operations example in Python
    This file demonstrates basic arithmetic operations in Python, including addition, subtraction, multiplication, division, exponentiation, floor division, and modulus. It also shows how to determine the data type of a variable.

---
+++ Operadores/op_arismetricos.py
@@ -0,0 +1,20 @@
+#suma y resta ( + y -)
+suma = 5 + 3
+resta = 5 - 3
+
+#multi y divisiones ( * y /):
+multi= 5 * 3
+division = 5 / 3 #devuelve un float siempre.
+
+#exponente ( ** ):
+exponente= 12 ** 2
+
+#double division ( // ):
+double_division = 5 // 3 #devuelve int siempre redondeando hacia abajo.
+
+#resto ( % ):
+resto = 5 % 3
+
+#type nos devuelve el tipo de dato.
+tipo_de_dato = type(double_division)
+print(tipo_de_dato) 
