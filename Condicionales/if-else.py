b02c5854117149aec844b21ea305f9d85839861b
Pablo Rojas <lizardrojasgil6.2@gmail.com>
2026-09-08 02:45:55 +0200

    Add if-else conditional examples in Python

---
+++ Condicionales/if-else.py
@@ -0,0 +1,23 @@
+#if condicion:
+#   accion:
+
+#if True:
+    #acción se ejecuta.
+
+#if False:
+    #acción no se ejecuta.
+
+edad= 17
+
+if edad >= 18:
+    print("podes pasar")
+else:
+    print("no podes pasar")
+    
+contraseña_almacenada = "1234"
+contraseña_ingresada = "1345"
+
+if contraseña_almacenada == contraseña_ingresada:
+    print("contraseña correcta")
+else:
+    print("contraseña incorrecta")
