b721f1a31f9b141376ea2ce27063c1bd31c84397
Pablo Rojas <lizardrojasgil6.2@gmail.com>
2026-09-08 02:30:26 +0200

    Document data types with examples in Datos_simples.py
    Added comments and examples for data types in Python.

---
+++ tipos de datos/Datos_simples.py
@@ -0,0 +1,22 @@
+"string" # texto o cadena de caracteres para solo una línea de texto
+'string' # texto o cadena de caracteres para solo una línea de texto
+# Example:
+#     "hola mundo"
+
+"""string""" # texto o cadena de caracteres para varias líneas de texto
+'''string''' # texto o cadena de caracteres para varias líneas de texto
+# Example:
+'''tus datos son:
+    nombre: Juan
+    edad: 30
+    email: juan@example.com'''
+    
+    
+int # número entero, sin decimales
+# Example: 40
+
+float # número decimal, con decimales
+# Example: 3.14
+
+bool # valor booleano, verdadero o falso
+# Example: True, False
