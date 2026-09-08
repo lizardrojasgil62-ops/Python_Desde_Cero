6f02f61886144cf428069b1fd7bee73d1372be92
Pablo Rojas <lizardrojasgil6.2@gmail.com>
2026-09-08 02:44:34 +0200

    Add comparison operators and example code
    Implement comparison operators and example usage.

---
+++ Operadores/op_comparación.py
@@ -0,0 +1,27 @@
+
+igual_que = 5 == 6
+
+distinto_que = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual_que = 5 >= 6
+
+menor_o_igual_que = 5 <= 6
+
+#calculos:
+
+a = 5
+b = 10
+c = 20
+comparacion = a + b == c
+
+print(comparacion)
+
+#contraseñas:
+contraseña_almacenada = "1234"
+contraseña_ingresada = "1345"
+
+print(contraseña_almacenada == contraseña_ingresada)
